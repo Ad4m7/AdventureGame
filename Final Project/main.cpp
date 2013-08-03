ecda8c267c2acd729b7acd99453ef9cb3cdcad2e
Adam Egyed <Adam@Adams-MacBook-Pro.local>
2013-08-02 18:30:04 -0700

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Adam Egyed on 8/2/13.
+//  Copyright (c) 2013 Adam Egyed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
